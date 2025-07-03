4d753637ee1b4840a29651d10a7d1a42aa1aa2e7
hihiads <98236975+hihiads@users.noreply.github.com>
2025-07-03 21:28:27 +0200

    Update serverroles.js

--- commands/serverroles.js
+++ commands/serverroles.js
@@ -1,1 +1,22 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+  name: 'serverroles',
+  description: 'Lists all roles on the server.',
+  aliases: ['roles', 'rolenames'],
+  execute(message, args) {
+    const roles = message.guild.roles.cache
+      .filter(role => role.name !== '@everyone')
+      .map(role => role.toString())
+      .join(', ') || 'No roles found.';
+
+    const embed = new MessageEmbed()
+      .setColor('#2f3136')
+      .setTitle(`Roles on ${message.guild.name}`)
+      .setDescription(roles)
+      .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }));
+
+    message.channel.send({ embeds: [embed] }).catch(console.error);
+  },
+};
 
