00a2ce7e207eaa26ab69e2580299caa4741cf941
hihiads <98236975+hihiads@users.noreply.github.com>
2025-07-06 01:27:25 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -9,19 +9,40 @@ module.exports = {
     const commandsPath = path.join(__dirname);
     const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js') && file !== 'help.js');
 
-    const embed = new MessageEmbed()
+    const embeds = [];
+    let embed = new MessageEmbed()
       .setTitle('📜 Available Commands')
       .setColor('#0099ff')
       .setDescription('Here is a list of all my commands with descriptions:')
       .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }))
       .setTimestamp();
 
+    let fieldCount = 0;
+
     for (const file of commandFiles) {
       const command = require(path.join(commandsPath, file));
+
+      // Dodaj polje
       embed.addField(`.${command.name}`, command.description || 'No description provided.');
+      fieldCount++;
+
+      // Ako je polja 25, pošalji embed i napravi novi
+      if (fieldCount === 25) {
+        embeds.push(embed);
+        embed = new MessageEmbed()
+          .setColor('#0099ff')
+          .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }))
+          .setTimestamp();
+        fieldCount = 0;
+      }
     }
 
-    message.channel.send({ embeds: [embed] });
+    // Dodaj zadnji embed ako ima polja
+    if (fieldCount > 0) embeds.push(embed);
+
+    // Pošalji sve embedove jedan za drugim
+    for (const e of embeds) {
+      await message.channel.send({ embeds: [e] });
+    }
   },
 };
-
